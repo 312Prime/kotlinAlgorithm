afea119e254d618eba8f97fc49a6f54bebe73a31
sangil <fhemflrh60@gmail.com>
2023-09-14 19:30:44 +0900

    부대 복귀 set

--- app/src/main/java/com/example/kotlinalgorithm/Main.kt
+++ app/src/main/java/com/example/kotlinalgorithm/Main.kt
@@ -8,37 +8,32 @@ fun main() {
     val lines = mutableListOf<String>()
     BufferedReader(FileReader(inputFile)).lines().forEach { lines.add(it) }
 
-    fun solution(picks: IntArray, minerals: Array<String>): Int {
-        var answer: Int = 0
-        val mList = mutableListOf<List<Int>>()
-        for (i in 1..picks[0] + picks[1] + picks[2]) {
-            minerals.slice((i * 5) - 5 until if (minerals.size < i * 5) minerals.size else i * 5)
-                .map {
-                    when (it) {
-                        "diamond" -> 25
-                        "iron" -> 5
-                        else -> 1
-                    }
-                }.also { if (it.isNotEmpty()) mList.add(it) }
+    fun solution(n: Int, roads: Array<IntArray>, sources: IntArray, destination: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        val cMap = mutableMapOf<Int, Int>()
+
+        for (i in 1..n) cMap[i] = -1
+        cMap[destination] = 0
+        roads.filter { it.first() == destination || it.last() == destination }.forEach {
+            if (it.first() == destination) cMap[it.last()] = 1
+            if (it.last() == destination) cMap[it.first()] = 1
         }
-        mList.sortBy { it.sum() }
-        for (i in picks.indices) {
-            for (j in 0 until picks[i]) {
-                when (i) {
-                    0 -> answer += (mList.removeLast().map { 1 }).sum()
-                    1 -> answer += (mList.removeLast().map { if (it == 25) 5 else 1 }).sum()
-                    2 -> answer += (mList.removeLast()).sum()
-                }
-                if (mList.isEmpty()) break
-            }
-            if (mList.isEmpty()) break
+        cMap.toList().sortedBy { it.second }.forEach {
+            it.first
         }
+
+        println(roads.sortedBy { it.first() })
+
         return answer
     }
 
     solution(
-        lines[0].removeSurrounding("[", "]").split(", ").map { it.toInt() }.toIntArray(),
-        lines[1].removeSurrounding("[", "]").split(", ").toTypedArray()
+        lines[0].toInt(),
+        lines[1].removeSurrounding("[[", "]]").split("], [").map { itt ->
+            itt.removeSurrounding("[", "]").split(", ").map { it.toInt() }.toIntArray()
+        }.toTypedArray(),
+        lines[2].removeSurrounding("[", "]").split(", ").map { it.toInt() }.toIntArray(),
+        lines[3].toInt(),
     ).also { print(it) }
     println()
 }
