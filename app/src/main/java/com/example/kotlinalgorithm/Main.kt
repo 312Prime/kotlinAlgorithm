8609f8e627d01e0e99a56fa0881275f42b73a256
sangil <fhemflrh60@gmail.com>
2023-09-22 19:30:49 +0900

    마법의 엘레베이터 set

--- app/src/main/java/com/example/kotlinalgorithm/Main.kt
+++ app/src/main/java/com/example/kotlinalgorithm/Main.kt
@@ -9,27 +9,13 @@ fun main() {
     val lines = mutableListOf<String>()
     BufferedReader(FileReader(inputFile)).lines().forEach { lines.add(it) }
 
-    fun solution(storey: Int): Int {
-        var answer = 0
-        var cNumber = storey
-        while (cNumber != 0) {
-            if (cNumber % 10 == 0) cNumber /= 10
-            if (cNumber % 10 < 5) {
-                answer += cNumber % 10
-                cNumber -= cNumber % 10
-            } else if (cNumber % 10 == 5 && cNumber % 100 <= 50) {
-                answer += cNumber % 10
-                cNumber -= cNumber % 10
-            } else {
-                answer += (10 - (cNumber % 10))
-                cNumber += (10 - (cNumber % 10))
-            }
-        }
+    fun solution(wallpaper: Array<String>): IntArray {
+        var answer: IntArray = intArrayOf()
         return answer
     }
 
     solution(
-        lines[0].toInt()
+        lines[0].removeSurrounding("[", "]").split(", ").toTypedArray()
     ).also { print(it) }
     println()
 }
