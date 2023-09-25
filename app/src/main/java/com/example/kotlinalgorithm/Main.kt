9e23c5e1333a2d85c314a4f31def7bfd93ef30f9
sangil <fhemflrh60@gmail.com>
2023-09-25 22:41:03 +0900

    code Add

--- app/src/main/java/com/example/kotlinalgorithm/Main.kt
+++ app/src/main/java/com/example/kotlinalgorithm/Main.kt
@@ -9,24 +9,6 @@ fun main() {
     BufferedReader(FileReader(inputFile)).lines().forEach { lines.add(it) }
 
     fun solution(inputs: Array<String>) {
-        val (N, cTime) = inputs[0].split(" ").map { it.toInt() }
-
-        val resultArray = mutableListOf<Pair<Int, Int>>()
-        for (i in 1 .. N) {
-            val values = inputs[i].split(" ")
-            val pair = Pair(values[0].toInt(), values[1].toInt())
-            resultArray.add(pair)
-        }
-
-        resultArray.sortBy { it.first }
-
-        var curTime = 0
-        for (person in resultArray) {
-            if (curTime < person.first) curTime = person.first
-            curTime += person.second
-            curTime += cTime
-        }
-        print(curTime)
     }
 
     solution(
