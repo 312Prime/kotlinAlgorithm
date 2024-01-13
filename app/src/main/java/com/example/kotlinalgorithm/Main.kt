27c7c25c365239de749d9d44d0b8a8a841aea4dc
sangil <fhemflrh60@gmail.com>
2024-01-13 23:59:36 +0900

    행렬의 곱셈

--- app/src/main/java/com/example/kotlinalgorithm/Main.kt
+++ app/src/main/java/com/example/kotlinalgorithm/Main.kt
@@ -11,13 +11,26 @@ fun main() {
     val lines = mutableListOf<String>()
     BufferedReader(FileReader(inputFile)).lines().forEach { lines.add(it) }
 
-    fun solution(array: IntArray, commands: Array<IntArray>) =
-        commands.map { array.toList().subList(it[0] - 1, it[1]).sorted()[it[2] - 1] }
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        var answer = arrayOf<IntArray>()
+
+        arr1.forEachIndexed { index, firstArray ->
+            val tempList = mutableListOf<Int>()
+            for (i in 0 until arr2[0].size) {
+//                tempList.add()
+                println(firstArray.indices.sumBy { number -> firstArray[number] * arr2.map { it[i] } })
+            }
+            answer += tempList.toIntArray()
+        }
+
+        return answer
+    }
 
     solution(
-        lines[0].split(", ").map { it.toInt() }.toIntArray(),
+        lines[0].split("], [").map { it.split(", ").map { it.toInt() }.toIntArray() }
+            .toTypedArray(),
         lines[1].split("], [").map { it.split(", ").map { it.toInt() }.toIntArray() }
             .toTypedArray(),
-    ).also { print(it.toMutableList()) }
+    ).also { print(it) }
     println()
 }
